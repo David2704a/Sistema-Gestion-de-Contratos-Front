09534c51d37b7ce9b330fa4b27e009f0013500f6
JohanB <davidbm270@gmail.com>
2024-11-20 14:23:36 -0500

    Renombre en la carpeta components

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -6,7 +6,6 @@ const Title = ({ title }) => {
             <div className="titleGlobal">
                 <div className="titleText">
                     <h1 className='textTitle'>{title}
-                        {/* <hr className='lineGlobal' /> */}
                     </h1>
                 </div>
             </div>
--- src/vistas/ContratosView.jsx
+++ src/vistas/ContratosView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/contratos.css'
-import Title from '../componentes/title';
+import Title from '../components/title';
 const ContratosView = () => {
     return (
         <div className={'ContratosView'}>

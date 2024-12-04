5d085e1212caa1b1cee6a088c1e48b9433591a9b
JohanB <davidbm270@gmail.com>
2024-12-04 08:29:09 -0500

    Se ajusto el datatable, con un componente directamente de react

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import DataTable from 'datatables.net-react';
-import DT from 'datatables.net-dt';
-
-DataTable.use(DT);
+// import DataTable from 'datatables.net-react';
+// import DT from 'datatables.net-dt';
+import DataTable from 'react-data-table-component';
+// DataTable.use(DT);
 
 const Table = ({ columns = [], data = [] }) => {
+    console.log(columns, 'alooo2', data, 'oaos2');
+    
     return (
         <div>
             <DataTable
--- src/vistas/UserView.jsx
+++ src/vistas/UserView.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Suspense } from 'react';
+import Table from '../components/table';
 
 const UserView = ({data}) => {
+
+   
+    
+    const columns = [
+      {
+        name: 'ID',
+        selector: row => row.id,
+        sortable: true,
+      },
+      {
+        name: 'Nombre',
+        selector: row => row.name,
+      },
+      {
+        name: 'Correo',
+        selector: row => row.email,
+      },
+    ];
+
+    const datas = data.users;
     return (
         <div>
             <h1>User View</h1>
+
+            <Table columns={columns} data={datas} />
         
-            <Suspense fallback={<div>Loading...</div>}>
+            {/* <Suspense fallback={<div>Loading...</div>}>
                 <ul className="card">
                     {data?.users?.map((user) => (
                         <li key={user.id}>{user.name}</li>
                     ))}
                 </ul>
-            </Suspense>
+            </Suspense> */}
         </div>
     );
 }
